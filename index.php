f58b7a43ce17beb6bd4faec7322680db27b1fff9
stealth35 <jinmoku@hotmail.com>
2012-07-03 15:47:49 +0200

    Add random function

--- index.php
+++ index.php
@@ -11,9 +11,16 @@ $finder->directories()->in(__DIR__)->exclude('vendor')->depth('== 0');
 $extensions = get_loaded_extensions();
 natcasesort($extensions);
 
+$functions = get_defined_functions();
+$functions = $functions['internal'];
+shuffle($functions);
+
+$function = current($functions);
+
 echo $twig->render('index.html.twig',array (
     'host'        => $_SERVER['SERVER_NAME'],
     'directories' => $finder,
     'infos'       => array(PHP_VERSION, PHP_SAPI, PHP_OS),
-    'extensions'  => $extensions
+    'extensions'  => $extensions,
+    'function'    => $function,
 ));
